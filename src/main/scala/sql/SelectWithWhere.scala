180d803c11b5bc560a1c38fe2211f63a7bb1ca49
tan-vu <tan.vu@lycorp.co.jp>
2026-01-04 02:11:26 +0900

    show query result

--- src/main/scala/sql/SelectWithWhere.scala
+++ src/main/scala/sql/SelectWithWhere.scala
@@ -11,6 +11,8 @@ object SelectWithWhere {
     val df = spark.sql("SELECT * FROM spark_db.users WHERE dt = '20241108' AND age > 30 ORDER BY name, age")
     df.explain(true)
 
+    df.show(10, truncate = false)
+
   }
 
 }
